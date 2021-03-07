33b6e6cb38c96c0dffa99cbeb288d70b131a6d87
ppapryczka <patryk.pankiewicz369@gmail.com>
2021-03-07 22:21:19 +0100

    Add arguments for python wrapper.

--- src/python_wrapper.cpp
+++ src/python_wrapper.cpp
@@ -10,9 +10,12 @@ using pybind11::literals::operator""_a;
 PYBIND11_MODULE(fault_injection_messages,m){
     py::class_<InjectionConfiguration>(m, "InjectionConfiguration");
 
-    m.def("get_InjectionConfiguration", &get_InjectionConfiguration);
+    m.def("get_InjectionConfiguration", &get_InjectionConfiguration,
+          "logging"_a, "register_injection_actions"_a, "memory_injection_actions"_a);
 
-    m.def("encode_InjectionConfiguration", &encode_InjectionConfiguration);
+    m.def("encode_InjectionConfiguration", &encode_InjectionConfiguration,
+          "configuration"_a);
 
-    m.def("decode_InjectionConfiguration", &decode_InjectionConfiguration);
+    m.def("decode_InjectionConfiguration", &decode_InjectionConfiguration,
+          "bytest_to_decode"_a);
 }
