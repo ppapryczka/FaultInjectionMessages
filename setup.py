3852cbd252e20efcc7e680ea47e7164de39d83a1
ppapryczka <patryk.pankiewicz369@gmail.com>
2021-03-07 21:54:40 +0100

    Change repository structure.

--- setup.py
+++ setup.py
@@ -103,13 +103,13 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="fault_injection_messages",
     version="0.0.1",
-    author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
+    author="Patryk Pankiewicz",
+    author_email="patryk.pankiewicz369@gmail.com",
     description="A test project using pybind11 and CMake",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("fault_injection_messages")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
 )
